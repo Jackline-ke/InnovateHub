a0e5c699e9fe468b8064fc40541001cd782deb74
Jackline-ke <antoninahodhiambo@gmail.com>
2024-02-16 10:22:57 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
+import { Navbar } from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className=''>
+      <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Link} from 'react-scroll'
+
+export const Navbar = () => {
+    const[nav, setNav] = useState(false)
+    const handleNav = ()=>{
+      setNav(!nav)
+    }
+    const closeNav = () => {
+        setNav(false);
+    } 
+  return (
+    <div className='fixed  w-full px-8 py-4 md:px-16 text-white font-serif bg-opacity-70 shadow-md' style={{ backdropFilter: 'blur(5px)', webkitBackdropFilter: 'blur(5px)', zIndex: 1000 /* Set a higher z-index */ }}>
+        <div className='flex justify-between items-center'>
+            <h1>InnovateHub</h1>
+            <div className='hidden lg:flex gap-8 cursor-pointer items-center'>
+                <Link to='home' smooth={true} duration={2000}>
+                    <h2>Home</h2>
+                </Link>
+                <Link to='features' smooth={true} duration={2000}>
+                    <h2>Services</h2>
+                </Link>
+                <Link to='skills' smooth={true} duration={2000}>
+                    <h2>Case Study</h2>
+                </Link>
+                <Link to='projects' smooth={true} duration={2000}>
+                    <h2>Pages</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000}>
+                    <h2>Blog</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000}>
+                    <h2>Contact</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000} className="px-3 py-1 bg-[#41cffc] rounded-md">
+                    <h2 className='text-[12px] text-white'>GET STARTED</h2>
+                </Link>
+            </div>
+            {/* humburger */}
+            <div onClick={handleNav} className='lg:hidden z-10'>
+                <FaBars size={20} className='cursor-pointer'/>
+            </div>
+            {/* mobile */}
+            <div onClick={handleNav} className={nav ? 'overflow-hidden lg:hidden ease-in duration-300 absolute text-[#f9f2ec] top-0 right-0  w-[40%] bg-[#4d3319]/90 px-4 py-16' : 'absolute left-[-100%] top-0 h-screen ease-in duration-500' }>
+                <div  className='flex flex-col gap-4 cursor-pointer text-lg'>
+                <Link to='home' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Home</h2>
+                </Link>
+                <Link to='features' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Features</h2>
+                </Link>
+                <Link to='skills' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Skills</h2>
+                </Link>
+                <Link to='projects' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Projects</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Contact</h2>
+                </Link>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
