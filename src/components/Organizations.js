2462394b7bf92afd95541d8b9e948bbed34a0215
Jackline-ke <antoninahodhiambo@gmail.com>
2024-02-20 11:53:31 +0300

    responsiveness

--- src/components/Organizations.js
+++ src/components/Organizations.js
@@ -5,7 +5,7 @@ const Organizations = () => {
     <div className='w-full h-full flex flex-col justify-center items-center font-serif mt-[630px] lg:mt-[450px] px-8 lg:px-0'>
         <div className='flex flex-col gap-6 justify-center items-center'>
             <h1 className='text-[#020e26] font-semibold text-2xl text-center'>Trusted With The World's Best Organizations</h1>
-            <div className='grid grid-cols-2 lg:flex justify-center items-center gap-4 lg:gap-32'>
+            <div className='grid grid-cols-2 md:flex justify-center items-center gap-4 lg:gap-32'>
               <img src='https://res.cloudinary.com/dmpr1moai/image/upload/v1708341594/innovateHub/microsoft_tzu32e.png'
                 alt=''
                 className='w-24 object-cover'
